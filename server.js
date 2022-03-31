b0e21077368085ff11a51b7cbd90f77ee049387d
David Samuelson <linksn.1fan@gmail.com>
2022-03-31 01:35:53 -0600

    added front end page functionality

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const { animals } = require('./data/animals.json');
 const fs = require('fs');
 const path = require('path');
+const { application } = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -94,6 +96,22 @@ app.post('/api/animals', (req, res) => {
     res.json(req.body);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+})
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
